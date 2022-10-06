68acf0656f3490072555eea7735359642933f8c2
patoayerve <patoayerve@outlook.com>
2022-10-06 13:25:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-a8s3tw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-a8s3tw)
---
+++ index.js
@@ -0,0 +1,6 @@
+ 
+ 
+ <h1> VISUALIZACION - AG1 </h1>
+
+ Nombre:  Cesar Patricio Ayerve Ramos
+ URL: <a href = 'https://js-a8s3tw.stackblitz.io'/>
